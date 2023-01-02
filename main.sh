aa01d23708cf430953350fc9c1462a0eb1a7dd27
SCL3 <106513368+SCL3@users.noreply.github.com>
2023-01-02 05:47:06 +0100

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,77 @@
+#!bin/bash
+
+help()
+{
+	#Display help
+	echo "This script filter and sort a meteo data csv file (specialy the one we are using in this project) depending on the option we are selecting"
+	echo
+	echo "Syntax : main.sh [option]..."
+	echo "Options :"
+	echo "--help     Show this help text" 
+	echo "-f [file]     Select the meteo data file"
+	echo
+}
+
+
+echo "Number of argument : "$#
+echo 
+
+verif=0  
+for i in $(seq 1 $#) ; do  #Check if the "-f" argument is used
+	ARG=${!i}
+	if [[ $ARG == '-f' ]]; then
+		(( file_value=$i+1 ))  #The next argument is the file name of the meteo data
+		file_name=${!file_value}
+		if [ -f "$file_name" ]; then  #Check if the meteo data file does exist
+			verif=1
+		else
+			verif=2
+		fi
+	fi
+done
+
+if [[ $verif -eq 0 ]]; then  #If the argument "-f" is not used :
+	echo "There is no meteo data file selected"
+	exit 2  #Exit the program
+elif  [[ $verif -eq 2 ]]; then  #Else if the meteo data file does not exist :
+	echo "The meteo data file does not exist"
+	exit 2
+fi
+
+for i in $(seq 1 $#) ; do  #Do the option for each arguments
+	ARG=${!i}  
+	case $ARG in
+		'--help') help  #Display help
+			;;
+		'-f' | $file_name)  #Do nothing to avoid displaying the unknown argument statement.
+			;;
+
+		'-t'*) echo "Temperature" ; case $ARG in  #Create a csv file depending of the mode chosen
+				'-t1') cut -d ';' -f 1,11,12,13 $file_name > temperature.csv  #Average temperature, minimum et maximum temperature depending of the station
+					;;
+				'-t2') cut -d ';' -f 2,11 $file_name > temperature.csv  #Average temperature depending of the time
+					;;
+				'-t3') cut -d ';' -f 1,2,11 $file_name > temperature.csv  #Average temperature depending of the time and the station
+					;;
+				*) echo "Unknown option, use --help for more details"	
+				esac
+			;;
+		'-p'*) echo "Atmospheric pressure" ; case $ARG in   #Create a csv file depending of the mode chosen
+				'-p1') cut -d ';' -f 1,7 $file_name > atmo_pressure.csv  #Average atmospheric pressure depending of the station
+					;;
+				'-p2') cut -d ';' -f 2,7 $file_name > atmo_pressure.csv  #Average atmospheric pressure depending of the time
+					;;
+				'-p3') cut -d ';' -f 1,2,7 $file_name > atmo_pressure.csv  #Average atmospheric pressure depending of the time and the station
+					;;
+				*) echo "Unknown option, use --help for more details"	
+				esac
+			;;
+		'-w') echo "Wind" ; cut -d ';' -f 1,4,5 $file_name > wind.csv #Create a csv file containing the average orientation and the average speed for each station.
+			;;
+		'-h') echo "Height" ; cut -d ';' -f 1,14 $file_name > height.csv  #Create a csv file containing the height of each station.
+			;;
+		'-m') echo "Moisture" ; cut -d ';' -f 1,6 $file_name > moisture.csv  #create a csv file containing the maximum humidity for each station.
+			;;
+		*) echo "Unknown argument, use --help for more details"  #The argument given is not treated.
+	esac
+done
