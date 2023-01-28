46536c5e38cca7b085a1812f659ac307c7e89e75
Mogramed <106923294+Mogramed@users.noreply.github.com>
2023-01-28 18:18:21 +0100

    Create Abr.h

---
+++ Abr.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+Abr* createAbr(int val, meteo);
+
+void infixeAbr_h(Abr* pTree);
+
+void infixeDecAbr_h(Abr* pTree);
+
+Abr* insert_Abr(Abr* pAbr, int a, Mto* meteo);
+
+int descending_csv_h(FILE* output ,Abr* height_abr);
